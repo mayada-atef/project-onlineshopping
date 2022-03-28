d46c8874ddb1b91ab30311f12d1427a73a84205e
mayada-atef <mayadaatef124@gmail.com>
2022-03-28 19:42:35 +0200

    edit4 m

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,8 +1,9 @@
 const categoryController = require("../controller/category.controller")
 const router = require("express").Router()
-router.post("/create", categoryController.create)
-router.delete("/delete/:id", categoryController.delete)
-router.patch("/edit/:id", categoryController.edit)
-router.get("/showall", categoryController.showAllcategories)
+const authAdmin=require("../middleware/authAdmin")
+router.post("/create",authAdmin, categoryController.create)
+router.delete("/delete/:id",authAdmin, categoryController.delete)
+router.patch("/edit/:id", authAdmin,categoryController.edit)
+router.get("/showall",categoryController.showAllcategories)
 
 module.exports = router
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -4,9 +4,11 @@ const router = require("express").Router()
 const authAdmin=require("../middleware/authAdmin")
 router.post("/addproduct",authAdmin,productController.addproduct)
 router.delete("/deleteproduct/:id",authAdmin, productController.deleteproduct)
-router.patch("/editproduct/:id",authAdmin, productController.editproduct)
-router.get("/showallproduct",authAdmin, productController.showALLproduct)
-router.get("/showsingleproduct/:id",authAdmin,productController.showproduct)
+router.patch("/editproduct/:id", authAdmin, productController.editproduct)
+// router.get("/showallproduct",authAdmin, productController.showALLproduct)
+// router.get("/showsingleproduct/:id", authAdmin, productController.showproduct)
+router.get("/showallproduct", productController.showALLproduct)
+router.get("/showsingleproduct/:id",productController.showproduct)
 
 
 
